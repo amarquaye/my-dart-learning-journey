29972f456c1321c68edea6455fe8194f92cdd1cd
Jesse Amarquaye <96346994+amarquaye@users.noreply.github.com>
2023-12-13 18:52:32 +0000

    Added strings.dart

---
+++ introduction/console-apps/strings.dart
@@ -0,0 +1,15 @@
+// Dart program to learn how to work with strings in dart.
+// By Jesse Amarquaye on Wednesday, 13th December 2023.
+
+void main() {
+  String characterName = "Oscar";
+  String characterAge = "64";
+
+  print("There was a man named ${characterName}");
+  print("He was ${characterAge} years old");
+
+  characterName = "Jesse";
+
+  print("He really liked the name ${characterName}");
+  print("But he didn't like being ${characterAge}");
+}
